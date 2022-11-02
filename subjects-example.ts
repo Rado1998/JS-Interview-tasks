a948de05c2ae091101dc9ac7abfe2a99647f507b
Radik Pogossian <radikpogossian@Radiks-MacBook-Pro.local>
2022-11-02 20:03:26 +0400

    updated subjects-example file

--- subjects-example.ts
+++ subjects-example.ts
@@ -1,16 +1,57 @@
-import { BehaviorSubject, Subject } from 'rxjs';
+import { BehaviorSubject, map, mergeMap, Observable, of, Subject, switchAll, switchMap } from 'rxjs';
 
-const subjectForEmit = new Subject();
-const subjectForListeners = new BehaviorSubject(subjectForEmit.asObservable());
+// const subjectForEmit = new Subject();
+// const subjectForListeners = new BehaviorSubject(subjectForEmit.asObservable());
 
-subjectForListeners.switch().subscribe((value) => {
-  console.log('value', value);
-});
+// subjectForListeners
+//   .pipe(
+//     switchAll()
+//   )
+//   .subscribe((value) => {
+//     console.log('value', value);
+//   });
+
+// subjectForEmit.next(20);
+
+// const secondSubjectForEmit = new Subject();
+
+// subjectForListeners.next(secondSubjectForEmit.asObservable());
+
+// secondSubjectForEmit.next('barev');
+
+// ==================================================================================//
+
+let subjectForEmit: Subject<string> | Subject<number> = new Subject<number>();
+const subjectForListeners = new BehaviorSubject<Observable<any> | null>(subjectForEmit.asObservable());
+
+function getSujectForListeners() {
+    return subjectForListeners
+        .pipe(
+            mergeMap((value: Observable<any> | null) => {
+                if (!value) {
+                    return of(null);
+                }
+                return value;
+            })
+        )
+}
+
+getSujectForListeners()
+    .subscribe((value) => {
+        console.log('value', value);
+    })
+
 
 subjectForEmit.next(20);
+subjectForEmit.next(30);
+subjectForEmit.next(40);
+subjectForEmit.next(50);
+subjectForEmit.next(60);
+subjectForEmit.next(70);
 
-const secondSubjectForEmit = new Subject();
+subjectForEmit = new Subject<string>();
+subjectForListeners.next(subjectForEmit.asObservable());
 
-subjectForListeners.next(secondSubjectForEmit.asObservable());
+subjectForEmit.next('barev');
+subjectForEmit.next('barev 1');
 
-secondSubjectForEmit.next('barev');
